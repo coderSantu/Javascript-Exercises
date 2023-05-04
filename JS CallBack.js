7e02003696ecf511c7b847b1365d73f0537ead93
Santu Das <Santuds71@gmail.com>
2023-05-04 06:29:08 +0530

    Example Callback

---
+++ JS CallBack.js
@@ -0,0 +1,10 @@
+function display(some){
+    console.log(some);
+}
+function calculator(num1, num2){
+    let sum = num1+num2;
+    return sum;
+}
+let result = calculator(5,5);
+
+display(result); //call
