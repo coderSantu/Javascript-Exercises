57a8e4b34de6a0b4efcd9e3f4973987f844221bc
Santu Das <Santuds71@gmail.com>
2023-05-11 23:32:42 +0530

    JS Function

---
+++ JS Set and Map.js
@@ -0,0 +1,14 @@
+const person ={
+    firstName: "Santu",
+    //Key: Value
+    lastName: "Das"
+}
+const fruits = new Map();
+fruits.set("apples", 300);
+fruits.set("Bananas", 400);
+fruits.set("Oranges", 200);
+
+fruits.set("apples", 600); //change the value
+
+
+console.log(fruits);
