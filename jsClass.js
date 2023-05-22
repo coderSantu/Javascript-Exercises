cbf9acab1ab659c16bc24351084244aebc35244d
Santu Das <Santuds71@gmail.com>
2023-05-22 11:25:34 +0530

    JavaScript Class

---
+++ jsClass.js
@@ -0,0 +1,10 @@
+class car{
+    constructor(name, year){
+        this.name=name;
+        this.year=year;
+    }
+}
+const tata = new car("Tata", 2020);
+console.log(tata);
+const maruti =new car("Maruti Suzuki", 2017);
+console.log(maruti);
