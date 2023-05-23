82b657e5c43439d4f1bfa6a6261a2a561cb6c414
Santu Das <91782076+coderSantu@users.noreply.github.com>
2023-05-23 22:46:13 +0530

    Object to JSON.js

--- object to JSON.js
+++ object to JSON.js
@@ -1,1 +1,12 @@
-Javascript object to JSON converting 
+//Javascript object to JSON converting 
+// Creating a JavaScript object
+var person = {
+  name: "Santu Das",
+  age: 30,
+  city: "Kolkata"
+};
+
+// Converting JavaScript object to JSON string
+var json = JSON.stringify(person);
+
+console.log(json);
