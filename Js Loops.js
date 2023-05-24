c9c682cc6e5d98a1f5e9ccc2c13dbbc10bf04425
Santu Das <91782076+coderSantu@users.noreply.github.com>
2023-05-24 07:02:13 +0530

    Javascript Loops

---
+++ Js Loops.js
@@ -0,0 +1,22 @@
+//1. For loop.
+
+for (var i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+//2. While loop
+
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+
+//3. Do while loop
+
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
+
